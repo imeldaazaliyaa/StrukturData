6e25c4dd90484c46457d7ca28141e03de5d8962d
imeldaazaliyaa <119922981+imeldaazaliyaa@users.noreply.github.com>
2023-03-08 20:07:09 +0700

    Delete Strukdatlat1 directory

--- Strukdatlat1/src/Latihan2/Makanan.java
+++
@@ -1,7 +0,0 @@
-package Latihan2;
-
-public class Makanan extends Hidangan{
-    public String disantap(){
-        return this.getNamaHidangan() + " dimakan";
-    }
-}
--- Strukdatlat1/src/Latihan2/Minuman.java
+++
@@ -1,7 +0,0 @@
-package Latihan2;
-
-public class Minuman extends Hidangan{
-    public String disantap(){
-        return this.getNamaHidangan() + " diminum";
-    }
-}
